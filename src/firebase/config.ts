ce59519457d4661a3e4d2d9c07bc8b4f22ed1c77
Uday Patil (Rising) <uday.patil.st@gmail.com>
2025-10-04 10:18:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-512789147-c299e",
+  "appId": "1:716970536385:web:b661f5e0c32c0f263f1340",
+  "apiKey": "AIzaSyAATc48_FJTXq58s0A1O1onwYzKvFrY5WU",
+  "authDomain": "studio-512789147-c299e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "716970536385"
+};
